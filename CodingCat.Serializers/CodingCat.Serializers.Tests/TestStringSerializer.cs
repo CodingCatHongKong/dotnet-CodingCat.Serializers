4fa44922a91af77d8b691d78c18b40601a6c1083
Coding Cat (Hong Kong) Limited <contact@codingcat.hk>
2020-02-15 17:03:48 +0800

    added test project with test cases

---
+++ CodingCat.Serializers/CodingCat.Serializers.Tests/TestStringSerializer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using CodingCat.Serializers.Impls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingCat.Serializers.Tests
+{
+    [TestClass]
+    public class TestStringSerializer
+    {
+        [TestMethod]
+        public void Test_ConvertWithDefaultEncoding_Ok()
+        {
+            // Arrange
+            var serializer = new StringSerializer();
+            var expected = Guid.NewGuid().ToString();
+            var bytes = serializer.ToBytes(expected);
+
+            // Act
+            var actual = serializer.FromBytes(bytes);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_ConvertWithASCIIEncoding_Ok()
+        {
+            // Arrange
+            var serializer = new StringSerializer()
+                .WithEncoding(Encoding.ASCII);
+            var expected = Guid.NewGuid().ToString();
+            var bytes = serializer.ToBytes(expected);
+
+            // Act
+            var actual = serializer.FromBytes(bytes);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ CodingCat.Serializers/CodingCat.Serializers.Tests/TestToStringSerializers.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using CodingCat.Serializers.Impls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingCat.Serializers.Tests
+{
+    [TestClass]
+    public class TestToStringSerializers
+    {
+        [TestMethod]
+        public void Test_JsonSerializer_Ok()
+        {
+            // Arrange
+            var serializer = new JsonSerializer<dynamic>();
+            var expected = new
+            {
+                Tick = DateTime.Now.Ticks.ToString()
+            };
+            var serialized = serializer.Serialize(expected);
+
+            // Act
+            var actual = serializer.Deserialize(serialized);
+
+            // Assert
+            Assert.AreEqual(expected.Tick, actual.Tick.ToString());
+        }
+
+        [TestMethod]
+        public void Test_Int16Serializer_Ok()
+        {
+            // Arrange
+            var serializer = new Int16Serializer();
+            var expected = (short)new Random().Next(
+                short.MinValue, short.MaxValue
+            );
+            var serialized = serializer.Serialize(expected);
+
+            // Act
+            var actual = serializer.Deserialize(serialized);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Int32Serializer_Ok()
+        {
+            // Arrange
+            var serializer = new Int32Serializer();
+            var expected = new Random().Next(
+                int.MinValue, int.MaxValue
+            );
+            var serialized = serializer.Serialize(expected);
+
+            // Act
+            var actual = serializer.Deserialize(serialized);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_BooleanSerializer_Ok()
+        {
+            // Arrange
+            var serializer = new BooleanSerializer();
+            var expectedTrue = true;
+            var expectedFalse = false;
+
+            var serializedTrue = serializer.Serialize(expectedTrue);
+            var serializedFalse = serializer.Serialize(expectedFalse);
+
+            // Act
+            var actualTrue = serializer.Deserialize(serializedTrue);
+            var actualFalse = serializer.Deserialize(serializedFalse);
+
+            // Assert
+            Assert.AreEqual(expectedTrue, actualTrue);
+            Assert.AreEqual(expectedFalse, actualFalse);
+        }
+    }
+}
